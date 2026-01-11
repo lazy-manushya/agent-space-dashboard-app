c285e32dbb57df7ebf9f2b0bd381d13c92bc2914
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2026-01-11 21:23:43 +0530

    Update DashboardPage.tsx

--- src/pages_lib/DashboardPage/DashboardPage.tsx
+++ src/pages_lib/DashboardPage/DashboardPage.tsx
@@ -1,16 +1,13 @@
 import { joinClassNames } from "@/utils";
-import Layout from "@/features/Layout";
 
 import { IDashboardPageProps } from "./DashboardPage.types";
 import styles from "./DashboardPage.module.css";
 
 function DashboardPage({ className }: IDashboardPageProps) {
   return (
-    <Layout>
-      <div className={joinClassNames(className, styles.Container)}>
-        Dashboard Page
-      </div>
-    </Layout>
+    <div className={joinClassNames(className, styles.Container)}>
+      Dashboard Page
+    </div>
   );
 }
 
