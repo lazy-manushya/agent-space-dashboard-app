ae646a6393cce908456877f463281a6a41225550
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2026-01-11 21:23:35 +0530

    Update NavMenu.tsx

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,12 +1,15 @@
 import { joinClassNames } from "@/utils";
 
+import Link from "@/components/Link";
+
 import { INavMenuProps } from "./NavMenu.types";
 import styles from "./NavMenu.module.css";
 
 export function NavMenu({ className }: INavMenuProps) {
   return (
     <div className={joinClassNames(className, styles.Container)}>
-      Nav Menu
+      <Link href="/">Home</Link>
+      <Link href="/users">Users</Link>
     </div>
   );
 }
