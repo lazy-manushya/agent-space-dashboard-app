93a233c5326168e829bc9a1c65ef417ec02c1f7c
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2026-01-13 12:30:33 +0530

    Update Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,7 +6,7 @@ import styles from "./Card.module.css";
 function Card({ className, title, children }: ICardProps) {
   return (
     <div className={joinClassNames(className, styles.Container)}>
-      <h3 className={styles.Title}>{title}</h3>
+      {!!title && <h3 className={styles.Title}>{title}</h3>}
       <div className={styles.Content}>{children}</div>
     </div>
   );
