584ba8438e627924979ea363d4a9d2757c1543e3
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2026-02-13 13:17:23 +0530

    Update Layout.tsx

--- src/features/Layout/Layout.tsx
+++ src/features/Layout/Layout.tsx
@@ -1,5 +1,7 @@
-import { joinClassNames } from "@/utils";
+"use client";
+import { usePathname } from "next/navigation";
 
+import { joinClassNames } from "@/utils";
 import NavMenu from "@/components/NavMenu";
 import Logo from "@/features/Logo";
 import AlertsButton from "@/features/AlertsButton";
@@ -8,7 +10,12 @@ import Button from "@/components/Button";
 import { ILayoutProps } from "./Layout.types";
 import styles from "./Layout.module.css";
 
+const EXPORT_SUMMARY_ROUTES = ["/", "/invoices", "/duties"];
+
 function Layout({ children, className }: ILayoutProps) {
+  const pathName = usePathname();
+  const showExportButton = EXPORT_SUMMARY_ROUTES.includes(pathName);
+
   return (
     <div
       className={joinClassNames(
@@ -49,7 +56,17 @@ function Layout({ children, className }: ILayoutProps) {
               ]}
             />
             {false && <AlertsButton />}
-            <Button>Export summary</Button>
+            {showExportButton && (
+              <Button variant="secondary">
+                <i
+                  className="las la-download"
+                  style={{
+                    fontSize: "1.25rem",
+                  }}
+                ></i>
+                Export summary
+              </Button>
+            )}
           </div>
         </header>
       </div>
