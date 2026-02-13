34c994817dab7f3fc3f71ae8bb610b8ec8b69c80
lazy-manushya <80703101+lazy-manushya@users.noreply.github.com>
2026-02-13 11:38:32 +0530

    Update card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,8 +6,10 @@ import styles from "./Card.module.css";
 function Card({ className, title, children, style }: ICardProps) {
   return (
     <div className={joinClassNames(className, styles.Container)} style={style}>
-      {!!title && <h3 className={styles.Title}>{title}</h3>}
-      <div className={styles.Content}>{children}</div>
+      <div className="flex-grow-1">
+        {!!title && <h3 className={styles.Title}>{title}</h3>}
+        <div className={styles.Content}>{children}</div>
+      </div>
     </div>
   );
 }
